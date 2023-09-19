3ad1cc6475749b551f32dcf20f33c5444dc9d4d9
Magali Courté <courte.magali@gmail.com>
2023-09-19 15:14:31 +0200

    Recherche tous les URL des livres de la catégorie Mystère

--- onecategory.py
+++ onecategory.py
@@ -13,23 +13,32 @@ from onebook_lien_fixe import extraire_donnees #Permet d'importer la fonction ex
 #Objectif : récuperer tous les livres d'une catégorie 
 
 links=[]
+url_cats=[]
+urls_onecat=[]
+ 
+#URL
+
+url= 'http://books.toscrape.com/catalogue/category/books/mystery_3/'
 
 for i in range(10) :
-    url_cat= 'http://books.toscrape.com/catalogue/category/books/mystery_3/page-' +str(i) +'.html'
-    response_cat = requests.get(url_cat)
+    url_onecat= url +'page-'+ str(i) +'.html'
+    response_cat = requests.get(url_onecat)
     if response_cat.ok :
+        urls_onecat.append(url_onecat)
         soup = BeautifulSoup(response_cat.text, 'lxml')
         h3s = soup.findAll('h3')
         for h3 in h3s :
             a=h3.find('a')   #on cherche tous les liens compris dans les "h3"
             link= a['href'].replace("../../../","")   #permet de supprimer les "../.." en début de chaque lien contenu dans h3
             links.append('http://books.toscrape.com/catalogue/' + link)   #rajoute à la liste links, le début fixe des URL + la partie que l'on vient de récupérer
-            
 
 
 with open('urls.txt', 'w') as file:
     for link in links:
         file.write(link +'\n')   #le +'\n` permet de passer à la ligne entre chaque nouveau lien
+
+
+   
 
 
 
