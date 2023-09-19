5ec12d12ff4e089d55b15762f3d6c61944963b6f
Magali Courté <courte.magali@gmail.com>
2023-09-19 12:26:18 +0200

    Ajout de commentaires

--- all_category.py
+++ all_category.py
@@ -28,12 +28,12 @@ if response.ok:
     soup = BeautifulSoup(response.text, 'lxml')
     if response.ok :
         soup = BeautifulSoup(response.text, 'lxml')
-        ul = soup.find('ul', class_='nav nav-list')
-        cat_ul=ul.find('ul')
-        list_cat=cat_ul.findAll('li')
+        ul = soup.find('ul', class_='nav nav-list') #permet de trouver les ul de la class indiquée
+        cat_ul=ul.find('ul') #Permet de trouver les ul compris dans la liste totale des ul = suppression du premier ul contenu dans a href
+        list_cat=cat_ul.findAll('li') #permet de lister tous les li compris dans la liste des ul précédemments sélectionnés
         for li in list_cat:
-            a=li.find('a')['href']
-            a_list.append(url + a)
+            a=li.find('a')['href']  #permet de récupérer le href de chacun des a
+            a_list.append(url + a)  #permet de concaténer l'url de base + les href qu'on a récupéré
 
 
 #Création d'un fichier texte avec l'ensemble des URLS des catégories
