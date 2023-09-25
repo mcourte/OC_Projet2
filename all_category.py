dd09e296f5a35c4783bba0fb217cc6d9d04e29d5
Magali Courté <courte.magali@gmail.com>
2023-09-25 22:26:23 +0200

    Récupère tous les URLs d'une catégorie

--- all_category.py
+++ all_category.py
@@ -2,12 +2,8 @@
 
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
-import csv
+import os
 
-import onebook_lien_fixe #Permet d'importer le script book sans avoir à recopier le code. Avantage : si modifications sur le script onebook, elles seront prises en compte
-from onebook_lien_fixe import extraire_donnees
 
 
 #Objectif : récuperer les liens de toutes les catégories
@@ -22,7 +18,8 @@ a=[]
 url ='http://books.toscrape.com/'
 
 #Début programme
-
+if not os.path.exists('Catégories') :
+                    os.mkdir('Catégories')
 response = requests.get(url)
 if response.ok:
     soup = BeautifulSoup(response.text, 'lxml')
@@ -33,13 +30,9 @@ if response.ok:
         list_cat=cat_ul.findAll('li') #permet de lister tous les li compris dans la liste des ul précédemments sélectionnés
         for li in list_cat:
             a=li.find('a')['href'].replace("index.html","")  #permet de récupérer le href de chacun des a
-            a_list.append(url + a)  #permet de concaténer l'url de base + les href qu'on a récupéré
-            
-
-
-#Création d'un fichier texte avec l'ensemble des URLS des catégories
-with open('all_urls.txt', 'w') as file:
-    for a in a_list:
-        file.write(a +'\n')   #le +'\n` permet de passer à la ligne entre chaque nouveau lien      
-       
+            name_cat=a.replace('catalogue/category/books/','').split('_')[0] 
+            noms_categorie ={'nom' : name_cat}
+            for value in noms_categorie.values():
+                if not os.path.exists('Catégories/%s' %value) :
+                    os.makedirs('Catégories/%s' %value)
 
