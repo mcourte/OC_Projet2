9a2ea21768d20011ca0b0b61e6e00628c3539aae
Magali Courté <courte.magali@gmail.com>
2023-10-05 15:39:03 +0200

    Fichier qui lance le programme

---
+++ main.py
@@ -0,0 +1,63 @@
+import sys
+import time
+
+
+import folder_by_categories
+import extract_data
+import extract_pict
+import choix_categories
+
+def convert(seconds):
+    min, sec = divmod(seconds, 60)
+    hour, min = divmod(min, 60)
+    return '%d:%02d:%02d' % (hour, min, sec)
+
+
+
+def main ():
+    url_base = "http://books.toscrape.com/"
+    while True:
+     print("\nMenu :")
+     print("1. Extraire toutes les catégories avec les images")
+     print("2. Extraire une catégorie spécifique avec les images")
+     print("3. Extraire les données d'un livre et son image")
+     print("4. Quitter")
+     choice = input("Choisissez une option : ")
+     if choice == "1":
+        start=time.time()
+        folder_by_categories.create_folder_category(url_base)
+        extract_data.extract_donnees_all_cat(url_base)
+        extract_pict.extract_pict_all_cat(url_base)
+        print('Fin du programme')
+        end_time=(time.time()-start)
+        print(convert(end_time))
+
+     elif choice == "2":
+        start=time.time()
+        url_cat=choix_categories.choix(url_base)
+        folder_by_categories.create_folder_one_category(url_cat)
+        extract_data.extract_donnees_one_cat(url_cat)
+        extract_pict.extract_pict_one_cat(url_cat)
+        print('Fin du programme')
+        end_time=(time.time()-start)
+        print(convert(end_time))
+
+     elif choice == "3":
+        start=time.time()
+        url_book=input("Entrez l'url du livre choisi : ")
+        folder_by_categories.create_folder_one_book(url_book)    
+        extract_data.extract_donnees_one_book(url_book)
+        extract_pict.extract_pict_one_book(url_book)
+        print('Fin du programme')
+        end_time=(time.time()-start)
+        print(convert(end_time))
+
+     elif choice == "4":
+        print("Arrêt du programme ! A bientôt")
+        break
+     else:
+      print("Choix invalide. Veuillez choisir une option valide.")
+
+
+if __name__=="__main__":
+   main()
